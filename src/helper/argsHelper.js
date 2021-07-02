bde31f67092bd92e45a5949f6ddbd4ff51476ed8
sschwei1 <24sebastian05@gmail.com>
2021-07-02 12:08:58 +0200

    more logging

--- src/helper/argsHelper.js
+++ src/helper/argsHelper.js
@@ -1,15 +1,17 @@
+const fh = require('fileHelper');
+
 const checkArgs = (msg, isArgs, haveArgs) => {
   // check if minimum amount of args is given
-  console.log('giv args', isArgs);
-  console.log('all args', haveArgs);
-  console.log('req args', haveArgs.filter(e => e.required));
-  if(isArgs.length < haveArgs.filter(e => e.required).length) {
+  const minArgs = haveArgs.filter(e => e.required);
+  if(isArgs.length < minArgs.length) {
+    fh.writeLog(`Received command from ${msg.author.name} with ${isArgs.length} arguments, expected a minimum of ${minArgs.length} arguments!`);
     msg.reply('Not enough arguments given');
     return false;
   }
 
   // check if maximum amount of args is not exceeded
   if(isArgs.length > haveArgs.length) {
+    fh.writeLog(`Received command from ${msg.author.name} with ${isArgs.length} arguments, expected a maximum of ${haveArgs.length} arguments!`);
     msg.reply('Too many arguments given');
     return false;
   }
